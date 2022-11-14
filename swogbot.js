c5b9104786c96fb996fd9a516726b8ac11dd2470
Ben Nelson <bgnelson25@gmail.com>
2022-11-13 22:16:47 -0600

    Update swogbot.js

--- swogbot.js
+++ swogbot.js
@@ -7,19 +7,19 @@ const clientId = config.clientId;
 
 const commands = [
   {
-    name: '$swog',
+    name: '\$swog',
     description: 'Swogs',
   },
   {
-    name: '$unswog',
+    name: '\$unswog',
     description: 'Unswogs',
   },
   {
-    name: '$swog status',
+    name: '\$swog status',
     description: 'Gets swog status',
   },
   {
-    name: '$swog help',
+    name: '\$swog help',
     description: 'Displays help',
   },
 ];
