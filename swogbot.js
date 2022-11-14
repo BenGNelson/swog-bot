3b3d3e80b8f2f92ca943a07ed9d1d4718b4c83a3
Ben Nelson <bgnelson25@gmail.com>
2022-11-13 21:44:57 -0600

    Updated main
    Changed to new format of interations

--- swogbot.js
+++ swogbot.js
@@ -3,82 +3,97 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const config = require('./config.json');
 
-const token = config.token
+const token = config.token;
 
 var swog = false;
 
 function setSwog(status) {
-    swog = status;
+	swog = status;
 }
 
-async function activateSwog(message, interaction) {
-    var number = Math.floor(Math.random() * 100);
+async function activateSwog(interaction) {
+	var number = Math.floor(Math.random() * 100);
 
-    if (number < 5) {
-        await interaction.reply("Swog unsuccessful. Please swog harder.");
-    } else {
-        await interaction.reply("Swog activated.");
-        await interaction.reply("Swog");
-        setSwog(true);
-        console.log("Swog status: " + swog);
-    }
+	if (number < 5) {
+		await interaction.reply('Swog unsuccessful. Please swog harder.');
+	} else {
+		await interaction.reply('Swog activated.');
+		await interaction.reply('Swog');
+		setSwog(true);
+		console.log('Swog status: ' + swog);
+	}
 }
 
-async function deactivateSwog(message, interaction) {
-    await interaction.reply("Swog deactivated.");
-    setSwog(false);
-    console.log("Swog status: " + swog);
+async function deactivateSwog(interaction) {
+	await interaction.reply('Swog deactivated.');
+	setSwog(false);
+	console.log('Swog status: ' + swog);
 }
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    
-    switch(interaction.commandName){
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
 
-        //swog
-        case '!swog':
+    if (interaction.commandName === '!swog') {
         if (!swog) {
-            activateSwog(message, interaction);
+            if (number < 5) {
+                await interaction.reply('Swog unsuccessful. Please swog harder.');
+            } else {
+                await interaction.reply('Swog activated.');
+                await interaction.reply('Swog');
+                setSwog(true);
+                console.log('Swog status: ' + swog);
+            }
+            activateSwog(interaction);
         } else {
-            await interaction.reply("Swog is already activated.");
+            await interaction.reply('Swog is already activated.');
         }
-        break;
+      }
 
-        //unswog
-        case '!unswog':
+      if (interaction.commandName === '!unswog') {
         if (!swog) {
-            await interaction.reply("Swog is already deactivated.");
+            await interaction.reply('Swog is already deactivated.');
         } else {
-            deactivateSwog(message, interaction);
+            await interaction.reply('Swog deactivated.');
+            setSwog(false);
+            console.log('Swog status: ' + swog);
         }
-        break;
+      }
 
-        //swog status
-        case '!swog status':
+      if (interaction.commandName === '!swog status') {
         if (swog) {
-            await interaction.reply("Swog is active.");
+            await interaction.reply('Swog is active.');
         } else {
-            await interaction.reply("Swog is not active. Type !swog to activate swog.");
+            await interaction.reply(
+                'Swog is not active. Type !swog to activate swog.'
+            );
         }
-        break;
+      }
 
-        //swog help
-        case '!swog help':
-        await interaction.reply({embed: {
-            color: 3447003,
-            title: "Swog Bot Options:",
-            fields: [
-              { name: "Command", value: "!swog\n!unswog\n!swog status", inline: true},
-              { name: "Description", value: "Activates swog\nDeactivates swog\nChecks the swog status", inline: true},
-            ]
-          }
+      if (interaction.commandName === '!swog help') {
+        await interaction.reply({
+            embed: {
+                color: 3447003,
+                title: 'Swog Bot Options:',
+                fields: [
+                    {
+                        name: 'Command',
+                        value: '!swog\n!unswog\n!swog status',
+                        inline: true,
+                    },
+                    {
+                        name: 'Description',
+                        value: 'Activates swog\nDeactivates swog\nChecks the swog status',
+                        inline: true,
+                    },
+                ],
+            },
         });
-    }
+      }
 });
 
 client.on('ready', () => {
-    console.log("Bot launched...");
-    client.user.setActivity('Swog: The Game');
+	console.log('Bot launched...');
+	client.user.setActivity('Swog: The Game');
 });
 
 client.login(token);
