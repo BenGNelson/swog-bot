ddde118d616edf99bbaf38ce744ae2fbdc259716
Ben Nelson <bgnelson25@gmail.com>
2022-11-13 22:10:10 -0600

    Update swogbot.js

--- swogbot.js
+++ swogbot.js
@@ -1,5 +1,10 @@
 const { REST, Routes } = require('discord.js');
 
+const config = require('./config.json');
+
+const token = config.token;
+const clientId = config.clientId;
+
 const commands = [
   {
     name: 'ping',
@@ -7,13 +12,13 @@ const commands = [
   },
 ];
 
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
   try {
     console.log('Started refreshing application (/) commands.');
 
-    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+    await rest.put(Routes.applicationCommands(clientId), { body: commands });
 
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
